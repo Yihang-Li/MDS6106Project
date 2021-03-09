5ee601ff981a09937032f1e1694fd4b2af3af8b1
Yihang Li <56984624+Yihang-Li@users.noreply.github.com>
2021-03-09 17:07:35 +0800

    Create py file for tuning parameter

---
+++ Pytorch_Version/Tune_Parameter.py
@@ -0,0 +1,8 @@
+# %%
+import sys
+sys.path.append("/Users/yihangli/Documents/GitHub/MDS6106Project")
+# %%
+from Pytorch_Version.Data_Prepare import *
+from Pytorch_Version.Model_Prepare import *
+import torch
+
